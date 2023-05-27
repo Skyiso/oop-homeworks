2f79b94a30bd5eb4f425bc260f366430d11d96e5
Irina Zhelovatykh <skyy.isoo@gmail.com>
2023-05-28 03:56:03 +0500

    Added grading methods

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -7,11 +7,32 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lec(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
+
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+
+class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
 
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -22,8 +43,23 @@ class Mentor:
         else:
             return 'Ошибка'
 
-class Lecturer(Mentor):
-    pass
 
-class Reviewer(Mentor):
-    pass
+first_student = Student('Ruoy', 'Eman', 'Male')
+first_student.courses_in_progress += ['Python']
+
+first_lecturer = Lecturer('Anthony', 'Merritt')
+first_lecturer.courses_attached += ['Python']
+
+first_reviewer = Reviewer('Edmund', 'Porter')
+first_reviewer.courses_attached += ['Python']
+
+first_reviewer.rate_hw(first_student, 'Python', 10)
+first_reviewer.rate_hw(first_student, 'Python', 7)
+first_reviewer.rate_hw(first_student, 'Python', 8)
+
+first_student.rate_lec(first_lecturer, 'Python', 10)
+first_student.rate_lec(first_lecturer, 'Python', 10)
+first_student.rate_lec(first_lecturer, 'Python', 10)
+
+print(first_student.grades)
+print(first_lecturer.grades)
