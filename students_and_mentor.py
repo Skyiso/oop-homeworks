73f4bb39d08b7a19ec488c7e01eb9806447cfa70
Irina Zhelovatykh <skyy.isoo@gmail.com>
2023-05-28 04:09:17 +0500

    Added method overrides

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -16,6 +16,32 @@ class Student:
         else:
             return 'Ошибка'
 
+    def _avg_rating(self):
+        # # Использование цикла
+        # sum_v = 0
+        # len_v = 0
+        # for value in self.grades.values():
+        #     sum_v += sum(value)
+        #     len_v += len(value)
+        # avg = round(sum_v / len_v, 1)
+        # return avg
+
+        # Короткое решение
+        list_of_raiting = sum(self.grades.values(), start=[])
+        avg = round(sum(list_of_raiting) /
+                    len(list_of_raiting), 1)
+        return avg
+
+    def __str__(self):
+        res = f'Имя: {self.name} \nФамилия: {self.surname} \nСредняя оценка за домашние задания: {self._avg_rating()} \nКурсы в процессе изучения: {", ".join(self.courses_in_progress)} \nЗавершенные курсы: {", ".join(self.finished_courses)}'
+        return res
+
+    def __lt__(self, other):
+        if not isinstance(other, Student):
+            print('Not a Student!')
+            return
+        return self._avg_rating() < other._avg_rating()
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -28,6 +54,22 @@ class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.grades = {}
+
+    def _avg_rating(self):
+        list_of_raiting = sum(self.grades.values(), start=[])
+        avg = round(sum(list_of_raiting) /
+                    len(list_of_raiting), 1)
+        return avg
+
+    def __str__(self):
+        res = f'Имя: {self.name} \nФамилия: {self.surname} \nСредняя оценка за лекции: {self._avg_rating()}'
+        return res
+
+    def __lt__(self, other):
+        if not isinstance(other, Lecturer):
+            print('Not a Lecturer!')
+            return
+        return self._avg_rating() < other._avg_rating()
 
 
 class Reviewer(Mentor):
@@ -43,12 +85,22 @@ class Reviewer(Mentor):
         else:
             return 'Ошибка'
 
+    def __str__(self):
+        res = f'Имя: {self.name} \nФамилия: {self.surname}'
+        return res
+
 
 first_student = Student('Ruoy', 'Eman', 'Male')
 first_student.courses_in_progress += ['Python']
+first_student.finished_courses += ['Git', 'SQL']
+second_student = Student('Jennifer', 'Stevenson', 'Female')
+second_student.courses_in_progress += ['Python', 'Git']
+second_student.finished_courses += ['SQL']
 
 first_lecturer = Lecturer('Anthony', 'Merritt')
 first_lecturer.courses_attached += ['Python']
+second_lecturer = Lecturer('Susan', 'Sherman')
+second_lecturer.courses_attached += ['Git']
 
 first_reviewer = Reviewer('Edmund', 'Porter')
 first_reviewer.courses_attached += ['Python']
@@ -56,10 +108,28 @@ first_reviewer.courses_attached += ['Python']
 first_reviewer.rate_hw(first_student, 'Python', 10)
 first_reviewer.rate_hw(first_student, 'Python', 7)
 first_reviewer.rate_hw(first_student, 'Python', 8)
+first_reviewer.rate_hw(second_student, 'Python', 9)
+first_reviewer.rate_hw(second_student, 'Python', 9)
+first_reviewer.rate_hw(second_student, 'Python', 10)
 
 first_student.rate_lec(first_lecturer, 'Python', 10)
 first_student.rate_lec(first_lecturer, 'Python', 10)
 first_student.rate_lec(first_lecturer, 'Python', 10)
+second_student.rate_lec(first_lecturer, 'Python', 9)
+second_student.rate_lec(second_lecturer, 'Git', 10)
+second_student.rate_lec(second_lecturer, 'Git', 10)
 
 print(first_student.grades)
+print(second_student.grades)
 print(first_lecturer.grades)
+print(second_lecturer.grades)
+
+print(first_student)
+print(second_student)
+print(first_student < second_student)
+print(first_student < second_lecturer)
+print(first_lecturer)
+print(second_lecturer)
+print(first_lecturer < second_lecturer)
+print(first_lecturer < second_student)
+print(first_reviewer)
